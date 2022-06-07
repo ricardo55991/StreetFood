b1410bb9d3eddf1cb59ac8187a89d4cc294dc527
Thalveex <thiago.alves0302@gmail.com>
2022-06-07 19:10:44 -0300

    Alteração na página de cadastro

--- pages/cadastre-se.php
+++ pages/cadastre-se.php
@@ -18,12 +18,24 @@
             </div>
             <div class="card-content">
                 <div class="card-content-area">
-                    <label for="usuario">Nome</label>
+                    <label for="nome">Nome</label>
                     <input type="text" id="usuario" autocomplete="off">
                 </div>
                 <div class="card-content-area">
-                    <label for="password">E-mail</label>
+                    <label for="cpf">CPF</label>
+                    <input type="text" id="cpf" autocomplete="off">
+                </div>
+                <div class="card-content-area">
+                    <label for="email">E-mail</label>
                     <input type="email" id="email" autocomplete="off">
+                </div>
+                <div class="card-content-area">
+                    <label for="ntelefone">Número de telefone</label>
+                    <input type="tel" id="ntelefone" autocomplete="off">
+                </div>
+                <div class="card-content-area">
+                    <label for="dnascimento">Data de nascimento</label>
+                    <input type="date" id="dnascimento" autocomplete="off">
                 </div>
                 <div class="card-content-area">
                     <label for="password">Senha</label>
