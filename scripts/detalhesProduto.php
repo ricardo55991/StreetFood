873b4608d625ed93701f9f7069386d2c3c4b5706
ricardo55991 <ricardo.barros.jr@gmail.com>
2022-06-09 00:38:21 -0300

    Ajuste divino

--- scripts/detalhesProduto.php
+++ scripts/detalhesProduto.php
@@ -19,13 +19,14 @@ if (isset($_SESSION['idProduto']) && !empty($_SESSION['idProduto']))
             <img class="img-fluid w-100 pb-1" src="'. $imagemProduto .'">
         </div>
         <div class="container col-lg-6 col-md-12 col-12">
-            <h2>'. $nomeProduto .'</h3>
+            <h2>'. $nomeProduto .'</h2>
                 <h3 class="text-bold fw-bolder fs-5">R$'. $precoProduto .'</h3>
                 <input type="number" value="1">
                 <button class="btn btn-warning mt-2 ">Adicionar ao pedido</button>
                 <h4 class="mt-4 mb-4 fw-bolder">Detalhes do produto</h4>
                 <span>'. $detalhesProduto .'</span>
                 <i class="bi bi-clipboard-pulse"></i>
+        </div>
         </div>
 </section>';
 
